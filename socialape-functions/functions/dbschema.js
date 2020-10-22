07e209f873b02079c718f26dddca7cb74e07c686
cleeag <jack0506000@gmail.com>
2020-10-22 11:06:05 -0700

    Express setup

---
+++ socialape-functions/functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: "this is the sceam body",
+            createdAt: "2020-10-22T18:00:55.635Z",
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
