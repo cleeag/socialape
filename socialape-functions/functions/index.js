246589ac3b8353192724c7e4d5f22445be2a66d5
cleeag <jack0506000@gmail.com>
2020-10-22 22:34:08 -0700

    image upload done

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -5,7 +5,7 @@ const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 
 const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login } = require('./handlers/user')
+const { signup, login, uploadImage } = require('./handlers/user')
 
 
 // Scream routes
@@ -14,9 +14,7 @@ app.post('/scream', FBAuth, postOneScream);
 
 // users routes
 app.post('/signup', signup);
-app.post('/login', login)
-
-// https://baseurl.com/screams is not good
-// https://baseurl.com/api/screams is better practice
+app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage);
 
 exports.api = functions.https.onRequest(app);
