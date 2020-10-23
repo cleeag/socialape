176dbd424424ca46e0d918d79de7e24bf4137448
cleeag <jack0506000@gmail.com>
2020-10-22 20:24:37 -0700

    done reorganizing code

---
+++ socialape-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyAyQD2mLdP7LVAd6kSGU4STfhQoWcO4UTA",
+    authDomain: "socialape-6193e.firebaseapp.com",
+    databaseURL: "https://socialape-6193e.firebaseio.com",
+    projectId: "socialape-6193e",
+    storageBucket: "socialape-6193e.appspot.com",
+    messagingSenderId: "778342482939",
+    appId: "1:778342482939:web:d28575616b2235cc85c15e",
+    measurementId: "G-QGYQDMP8T6"
+}
