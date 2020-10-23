534b7e051f7053324609af4476b78ead0f81f7f8
cleeag <jack0506000@gmail.com>
2020-10-22 18:21:48 -0700

    Authentication Middleware completed

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -41,15 +41,48 @@ app.get('/screams', (req, res) => {
         .catch((err) => console.error(err));
 });
 
-app.post('/scream', (req, res) => {
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No Token Found');
+        res.status(403).json({ error: 'Unauthorized' });
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            console.log(decodedToken);
+            console.log(req.user.uid);
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch(err => {
+            console.error('Error while verifying token ', err);
+            return res.status(403).json(err);
+        })
+}
+
+// Post one scream
+app.post('/scream', FBAuth, (req, res) => {
+    if (req.body.body.trim() === '') {
+        return res.status(400).json({ body: 'Body must not be empty' });
+    }
+
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     };
 
-    db
-        .collection('screams')
+    db.collection('screams')
         .add(newScream)
         .then(doc => {
             res.json({ message: `document ${doc.id} created successfully` });
@@ -93,7 +126,7 @@ app.post('/signup', (req, res) => {
     if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
     // TODO: validate data
-    db.doc(`/user/${newUser.handle}`).get()
+    db.doc(`/users/${newUser.handle}`).get()
         .then(doc => {
             if (doc.exists) {
                 return res.status(400).json({ handle: 'this handle is already taken' });
@@ -113,7 +146,7 @@ app.post('/signup', (req, res) => {
                 createdAt: new Date().toISOString(),
                 userId
             };
-            return db.doc(`/user/${newUser.handle}`).set(userCredentials);
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
         })
         .then(() => {
             return res.status(201).json({ token });
@@ -151,8 +184,8 @@ app.post('/login', (req, res) => {
         })
         .catch(err => {
             console.error(err);
-            if(err.code == "auth/wrong-password"){
-                return res.status(403).json({ general: "Wrong credentials, please try again"})
+            if (err.code == "auth/wrong-password") {
+                return res.status(403).json({ general: "Wrong credentials, please try again" })
             } else return res.status(500).json({ error: err.code });
         })
 
